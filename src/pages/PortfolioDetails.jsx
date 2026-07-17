c2f97943dc9aa4d896601bf5eb5c09dae325941f
Puthik15 <puthik1155@gmail.com>
2026-07-17 23:55:45 +0700

    Add missing PortfolioDetails and ServiceDetails page components

---
+++ src/pages/PortfolioDetails.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function PortfolioDetails() {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-8">
+          <h1>Portfolio Details</h1>
+          <p>Portfolio details page content coming soon.</p>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/pages/ServiceDetails.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function ServiceDetails() {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-8">
+          <h1>Service Details</h1>
+          <p>Service details page content coming soon.</p>
+        </div>
+      </div>
+    </div>
+  );
+}
