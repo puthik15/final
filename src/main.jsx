f52b1c2f7dc16eb46b18d2cb7980bb761720ac92
Puthik15 <puthik1155@gmail.com>
2026-07-17 21:22:41 +0700

    Remove static detail pages (migrated to React routes)

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
+import PortfolioDetails from "./pages/PortfolioDetails";
+import ServiceDetails from "./pages/ServiceDetails";
 import "./App.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/portfolio-details" element={<PortfolioDetails />} />
+        <Route path="/service-details" element={<ServiceDetails />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 );
